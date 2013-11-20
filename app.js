23a2919c02137b1184b67b62169d60534f95447b
Daniel Hug <danielthehug@gmail.com>
2013-11-20 14:45:10 -0800

    Get rid of temp move element

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-(function() {
-	'use strict';
-
 // Array-like object to Array:
 function toArray(o) {
 	for (var a = [], l=o.length; l--;) a[l] = o[l];
@@ -13,6 +10,16 @@ function $$(s, el) {
 }
 function $(s, el) {
 	return (el || document).querySelector(s);
+}
+
+// Get element position:
+function getOffset(el) {
+	return {
+		top: el.offsetTop,
+		left: el.offsetLeft,
+		bottom: el.offsetTop + el.offsetHeight,
+		right: el.offsetLeft + el.offsetWidth
+	};
 }
 
 // Which keys are pressed:
@@ -51,6 +58,7 @@ var Game = function(viewportEl) {
 	t.score = 0;
 	this.finished = false;
 	t.player = {};
+	t.step = 2;
 
 	// Viewport element & dimensions:
 	t.viewport = {
@@ -63,23 +71,21 @@ var Game = function(viewportEl) {
 	t.solidEls  = $$('.solid',  viewportEl);
 	t.coinEls   = $$('.coin',   viewportEl);
 	t.scoreEls  = $$('.score',  viewportEl);
-	t.finishEl  =  $(".finish", viewportEl);
+	t.finishI  =  t.solidEls.indexOf($(".finish", viewportEl));
 	t.player.el =  $(".player", viewportEl);
+	
+	// Cache positions of solids:
+	t.solids = t.solidEls.map(getOffset);
+	
+	// Cache coin positions:
+	t.coins = t.coinEls.map(getOffset);
 
-	// Player position data:
-	t.player.x      = t.player.el.offsetLeft;
-	t.player.y      = t.player.el.offsetTop;
-	t.player.width  = t.player.el.offsetWidth;
-	t.player.height = t.player.el.offsetHeight;
-
-	// Adds a new div.tmp-move which is used to calculate whenever the player is allowed to move:
-	t.tmpMovEl = document.createElement('div');
-	t.tmpMovEl.className = "tmp-move";
-	t.viewport.el.insertBefore(t.tmpMovEl, t.viewport.el.firstChild);
-	t.tmpMovEl.style.left = t.player.x + 'px';
-	t.tmpMovEl.style.top = t.player.y + 'px';
-	t.tmpMovEl.style.width = t.player.width + 'px';
-	t.tmpMovEl.style.height = t.player.height + 'px';
+	// Cache player position:
+	t.player.pos = {};
+	t.player.pos.top  = t.player.el.offsetTop;
+	t.player.pos.left = t.player.el.offsetLeft;
+	t.player.pos.bottom = t.player.pos.top + t.player.el.offsetHeight;
+	t.player.pos.right  = t.player.pos.left + t.player.el.offsetWidth;
 
 	// Setup interval. Delay controlls tickrate:
 	setInterval(function() {
@@ -100,40 +106,32 @@ Game.prototype.setScore = function(method, amount) {
 
 
 // Checks if an element is inside its viewport:
-Game.prototype.insideGameArea = function(el) {
-
-	// element position:
-	var x = el.offsetLeft;
-	var y = el.offsetTop;
-
+Game.prototype.insideGameArea = function(offset) {
 	return !(
-		x < 0 ||
-		y < 0 ||
-		x + this.player.width  > this.viewport.width ||
-		y + this.player.height > this.viewport.height
+		offset.left < 0 ||
+		offset.top  < 0 ||
+		offset.right  > this.viewport.width ||
+		offset.bottom > this.viewport.height
 	);
 };
 
 // Checks if element a overlaps element b
-Game.prototype.elOverlap = function(a, b) {
-	var aX = a.offsetLeft;
-	var aY = a.offsetTop;
-	var bX = b.offsetLeft;
-	var bY = b.offsetTop;
-
+Game.prototype.elOverlap = function(a, b) {	
 	// a and b overlap if a is not left, right, above, or below b:
 	return !(
 		// Is a to the left of b?
-		bX >= aX + a.offsetWidth ||
+		a.right <= b.left ||
+		//b.left >= a.right ||
 
 		// Is a to the right of b?
-		bX + b.offsetWidth <= aX ||
+		b.right <= a.left ||
 
 		// Is a above b?
-		bY >= aY + a.offsetHeight ||
+		a.bottom <= b.top ||
+		//b.top >= a.bottom ||
 
 		// Is a below b?
-		bY + b.offsetHeight <= aY
+		b.bottom <= a.top
 	);
 };
 
@@ -141,63 +139,64 @@ Game.prototype.elOverlap = function(a, b) {
 // Move one pixel for each direction and check if move is valid.
 Game.prototype.movTick = function() {
 	var moved = false;
-	var s  = this.tmpMovEl.style;
-	var tCS = getComputedStyle(this.tmpMovEl);
+	var t = this;
+	
+	var offset = {
+		top: t.player.pos.top,
+		left: t.player.pos.left,
+		bottom: t.player.pos.bottom,
+		right: t.player.pos.right
+	};
 
 	if (!(keys.up && keys.down)) {
-		if (keys.up)    { s.top  = parseInt(tCS.top,  10) - 1 + 'px'; moved = true; }
-		if (keys.down)  { s.top  = parseInt(tCS.top,  10) + 1 + 'px'; moved = true; }
+		if      (keys.up)    { offset.top -= t.step; offset.bottom -= t.step; moved = true; }
+		else if (keys.down)  { offset.top += t.step; offset.bottom += t.step; moved = true; }
 	}
 	if (!(keys.left && keys.right)) {
-		if (keys.left)  { s.left = parseInt(tCS.left, 10) - 1 + 'px'; moved = true; }
-		if (keys.right) { s.left = parseInt(tCS.left, 10) + 1 + 'px'; moved = true; }
+		if      (keys.left)  { offset.left -= t.step; offset.right -= t.step; moved = true; }
+		else if (keys.right) { offset.left += t.step; offset.right += t.step; moved = true; }
 	}
 
 	if (!moved) return;
 	var moveAllowed = true;
 
 	// Check if move is inside the game area:
-	if (this.insideGameArea(this.tmpMovEl)) {
+	if (t.insideGameArea(offset)) {
 
 		// Solid collision:
-		this.solidEls.forEach(function(solidEl) {
-			if (this.elOverlap(this.tmpMovEl, solidEl)) {
+		t.solids.forEach(function(solidPos, i) {
+			if (this.elOverlap(offset, solidPos)) {
 				moveAllowed = false; // Checks if there is any overlap on a solid object.
 
 				// Finish collision:
-				if (!this.finished && solidEl === this.finishEl) {
+				if (!this.finished && i === this.finishI) {
 					this.setScore('add', 250);
 					this.finished = true;
 				}
 			}
-		}, this);
+		}, t);
 
 		// Coin collision:
-		this.coinEls.forEach(function(coinEl) {
-			if (this.elOverlap(this.tmpMovEl, coinEl)) {
+		t.coins.forEach(function(coinPos, i) {
+			if (this.elOverlap(offset, coinPos)) {
 				this.setScore('add', 50);
-				coinEl.parentNode.removeChild(coinEl);
+				delete this.coins[i];
+				this.coinEls[i].parentNode.removeChild(this.coinEls[i]);
 			}
-		}, this);
+		}, t);
 
 	} else {
 		moveAllowed = false;
 	}
 
-	var ps = this.player.el.style,
-		ts = this.tmpMovEl.style,
-		pCS = getComputedStyle(this.player.el);
+	var ps = t.player.el.style;
 	if (moveAllowed) { // Move player:
-		ps.left = tCS.left;
-		ps.top  = tCS.top;
-	} else { // Reset the tmpMovEl to last location:
-		ts.left = pCS.left;
-		ts.top  = pCS.top;
+		ps.left = offset.left + 'px';
+		ps.top  = offset.top + 'px';
+		t.player.pos = offset;
 	}
 };
 
 
 // Create a game:
 var game = new Game($(".game"));
-
-})();
